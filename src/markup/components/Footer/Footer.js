84e455f2f42f25d62f9d76bac11f8949fc0a3d0b
Jhan-hab <201286123+yontanhabtmu45@users.noreply.github.com>
2025-06-09 13:50:22 +0300

    final upload

--- src/markup/components/Footer/Footer.js
+++ src/markup/components/Footer/Footer.js
@@ -62,7 +62,7 @@ function Footer(props) {
                         <ul className="list">
                           <li><Link to="/">Home</Link></li>
                           <li><Link to="/about">About Us</Link></li>
-                          <li><Link to="#">Appointment</Link></li>
+                          <li><Link to="/services">Services</Link></li>
                           <li><Link to="/contact">Contact Us</Link></li>
                         </ul>
                       </div>
